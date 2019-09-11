5586a64006154ad7d11ee1146a76810d894c70ef
Scott Flicker <sflicker2@earthlink.net>
2019-09-11 00:06:17 -0400

    work in progress

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Total from './components/Total';
 import CalcButtons from './components/CalcButtons';
 import SimpleLayout from './calclayouts/SimpleLayout';
-import uuid from 'uuid';
 
 import './App.css';
 
@@ -53,27 +52,44 @@ class App extends Component {
   }
   render() {
 
-    let buttons=[];
+    const button_rows=[];
 
-    for (var i=0;i<=9;i++) {
-      buttons.push( {btn: {id: uuid.v4(), value: i}} );
-    };
-    buttons.push({btn: {id: uuid.v4(), value: "+"}} );
-    buttons.push({btn: {id: uuid.v4(), value: "-"}} );
-    buttons.push({btn: {id: uuid.v4(), value: "*"}} );
-    buttons.push({btn: {id: uuid.v4(), value: "/"}} );
-    buttons.push({btn: {id: uuid.v4(), value: "."}} );
-    buttons.push({btn: {id: uuid.v4(), value: "="}} );
-    buttons.push({btn: {id: uuid.v4(), value: 'C'}} );
-    buttons.push({btn: {id: uuid.v4(), value: 'AC'}} );
-
+    button_rows.push([
+      {btn: {id: "OP_CLEAR", value: 'C'}},
+      {btn: {id: "OP_ALL_CLEAR", value: 'AC'}}
+     ]);
+    button_rows.push( [  
+        {btn: {id: "NUM_1", value: "1"}},
+        {btn: {id: "NUM_2", value: "2"}},
+        {btn: {id: "NUM_3", value: "3"}},
+        {btn: {id: "OP_MULTI", value: "*"}}
+      ]);
+    button_rows.push( [
+        {btn: {id: "NUM_4", value: "4"}},
+        {btn: {id: "NUM_5", value: "5"}},
+        {btn: {id: "NUM_6", value: "6"}},  
+        {btn: {id: "OP_DIV", value: "/"}}
+      ]);
+    button_rows.push( [
+        {btn: {id: "NUM_7", value: "7"}},
+        {btn: {id: "NUM_8", value: "8"}},
+        {btn: {id: "NUM_9", value: "9"}},
+        {btn: {id: "OP_SUB", value: "-"}}
+    ]);
+    button_rows.push( [
+      {btn: {id: "NUM_0", value: "0"}},
+      {btn: {id: "OP_DECIMAL", value: "."}},
+      {btn: {id: "OP_EQUAL", value: "="}},
+      {btn: {id: "OP_PLUS", value: "+"}}
+     ]);
+  
+    console.log("button_rows.length=" + button_rows.length);
 
     return (
       <div>
         <h1>Calculator</h1>
         <Total total={this.state.currValue} />
-        <SimpleLayout></SimpleLayout>
-        <CalcButtons onClick={this.onBtnClick} buttons={buttons} />
+        <CalcButtons onClick={this.onBtnClick} buttons={button_rows} />
       </div>
     );
   }
--- src/components/CalcButtons.js
+++ src/components/CalcButtons.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import CalcButton from './CalcButton';
 
+const makeButtonRow = (button_row, onClick) => {
+    console.log("makeButtonRow");
+    console.log("button_row.length=" + button_row.length);
+    return button_row.map((button) => (
+            <div>
+                <CalcButton onClick={onClick} settings={button} />
+            </div>
+    ));
+}
+
 class CalcButtons extends Component {
     render() {
         return (
             <div>
-                {this.props.buttons.map((button) => (
-                    <CalcButton onClick={this.props.onClick} settings={button} />
+                {this.props.buttons.map((button_row) => (
+                    <div class="calculator-row">
+                        {makeButtonRow(button_row, this.props.onClick)}
+                    </div>
                 )) }
             </div>
         )
